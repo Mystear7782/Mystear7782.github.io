f0bad6f2c5245f36dcc048c29e9d7b60bf815283
Mystear7782 <msr.r2.charlotte@gmail.com>
2026-03-21 21:36:33 +0900

    キャッシュ問題の修正2

--- sw.js
+++ sw.js
@@ -1,23 +1,49 @@
 const CACHE = 'gymlog-v3';
 const ASSETS = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
+
+// HTMLとJSは常にネットワーク優先、オフライン時のみキャッシュ
+// フォント等の外部リソースはキャッシュ優先
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
+  const url = new URL(e.request.url);
+  const isLocal = url.origin === self.location.origin;
+  const isPage = isLocal && (
+    /\.(html|js)$/.test(url.pathname) ||
+    url.pathname === '/' ||
+    url.pathname.endsWith('/')
+  );
+
+  if (isPage) {
+    e.respondWith(
+      fetch(e.request).then(res => {
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(cached =>
+        cached || fetch(e.request).then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+      )
+    );
+  }
 });
